13dd3304db4bff599b6c977d51e44a2c631d904e
headcha <headcha@naver.com>
2014-10-12 19:54:57 +0900

    서버 개발완료

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 (function() {
 	var io;
+	var VIEW_SOCKET_GROUP_NAME = "viewSocketsRoom";
 	io = require('socket.io').listen(4000);
+	
 	io.sockets.on('connection', function(socket) {
-		socket.emit('news', {
-			hello : 'world'
+		
+		socket.on('join' , function() {
+			socket.join(VIEW_SOCKET_GROUP_NAME);
 		});
-		socket.on('my other event', function(data) {
-			console.log(data);
+		
+		socket.on('sendMessage', function(data) {
+			io.to(VIEW_SOCKET_GROUP_NAME).emit('onMessageReceiver' , data);
+			socket.emit('complate');
 		});
 	});
 }).call(this);
